e8b7a8d6ceb13507719b21ebb8c913fb962611d7
Zihadul Islam <22-47087-1@student.aiub.edu>
2026-09-05 04:29:24 +0600

    Clean CourseRepo and remove legacy interface stubs

--- DAL/Repos/CourseRepo.cs
+++ DAL/Repos/CourseRepo.cs
@@ -1,72 +1,57 @@
-﻿using DAL.EF;
+using DAL.EF;
 using DAL.EF.Model;
-using DAL.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    public class CourseRepo : IRepo<Course, int, Course>, ICourseFeatures
+    public class CourseRepo
     {
-        LMSContext db;
-
-        public CourseRepo()
-        {
-        }
+        private readonly LMSContext db;
 
         public CourseRepo(LMSContext db)
         {
-            this.db = db;
+            this.db = db ?? throw new ArgumentNullException(nameof(db));
         }
-        public bool Create(Course c)
-        {
-            db.Courses.Add(c);
-            return db.SaveChanges() > 0;
 
+        public bool Create(Course course)
+        {
+            db.Courses.Add(course);
+            return db.SaveChanges() > 0;
         }
+
         public bool Delete(int id)
         {
-            var ex = Get(id);
-            db.Courses.Remove(ex);
+            var existing = Get(id);
+            if (existing == null) return false;
+
+            db.Courses.Remove(existing);
             return db.SaveChanges() > 0;
         }
+
         public Course Get(int id)
         {
             return db.Courses.Find(id);
         }
+
         public List<Course> Get()
         {
             return db.Courses.ToList();
         }
+
         public List<Course> GetCoursesSortedByDuration()
         {
             return db.Courses.OrderByDescending(c => c.Duration).ToList();
         }
 
-        public bool Update(Course c)
+        public bool Update(Course course)
         {
-            var ex = Get(c.Id);
-            db.Entry(ex).CurrentValues.SetValues(c);
+            var existing = Get(course.Id);
+            if (existing == null) return false;
+
+            db.Entry(existing).CurrentValues.SetValues(course);
             return db.SaveChanges() > 0;
-           
         }
-
-        Course IRepo<Course, int, Course>.Create(Course obj)
-        {
-            throw new NotImplementedException();
-        }
-
-        Course IRepo<Course, int, Course>.Update(Course obj)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    internal interface ICourseFeatures
-    {
     }
 }
-

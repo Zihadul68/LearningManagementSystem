2c9bdd0272ce1b62c6e311be57237df44151add0
Zihadul Islam <22-47087-1@student.aiub.edu>
2026-09-05 04:23:09 +0600

    Fix EnrollmentController update call and clean controller

--- LearningManagementSystem/Controllers/EnrollmentController.cs
+++ LearningManagementSystem/Controllers/EnrollmentController.cs
@@ -1,5 +1,4 @@
-﻿using BLL.Services;
-using Microsoft.AspNetCore.Http;
+using BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LearningManagementSystem.Controllers
@@ -8,50 +7,46 @@ namespace LearningManagementSystem.Controllers
     [ApiController]
     public class EnrollmentController : ControllerBase
     {
-        EnrollmentService service;
+        private readonly EnrollmentService service;
+
         public EnrollmentController(EnrollmentService service)
         {
             this.service = service;
         }
+
         [HttpGet("all")]
         public IActionResult All()
         {
-            {
-                var data = service.Get();
-                return Ok(data);
-            }
+            var data = service.Get();
+            return Ok(data);
         }
+
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
             var data = service.Get(id);
             return Ok(data);
         }
+
         [HttpPost("create")]
         public IActionResult Create(BLL.DTOs.EnrollmentDTO e)
         {
             var data = service.Create(e);
             return Ok(data);
         }
+
         [HttpPut("update")]
         public IActionResult Update(BLL.DTOs.EnrollmentDTO e)
         {
-            var data = service.update(e);
+            var data = service.Update(e);
             return Ok(data);
         }
+
         [HttpDelete("delete/{id}")]
         public IActionResult Delete(int id)
         {
             var data = service.Delete(id);
             return Ok(data);
         }
-        //[HttpGet("courses-with-enrolled-students")]
-
-        //public async Task<IActionResult> GetCoursesWithEnrolledStudents()
-        //{
-        //    var data = await service.GetCoursesWithEnrolledStudentsAsync(); // implement async service
-        //    if (data == null || !data.Any()) return NoContent();
-        //    return Ok(data);
-        //}
     }
 }
